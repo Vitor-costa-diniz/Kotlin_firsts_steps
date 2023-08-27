0dc5f3718ed569251fffe6347261b1861c962264
Vitor-costa-diniz <78705027+Vitor-costa-diniz@users.noreply.github.com>
2023-08-27 11:10:04 -0300

    operators

---
+++ Aprendendo_Kotlin/app/src/main/java/com/vitorcosta/aprendendokotlin/Operadores.kt
@@ -0,0 +1,13 @@
+package com.vitorcosta.aprendendokotlin
+
+fun main() {
+//    val n1 = 10
+//    val n2 = 20
+
+    val idadePessoa = 50
+    val comprasPessoa = 100
+
+//    val resultado = 2 > 1 && 4 ==3
+    val resultado = idadePessoa >= 50 || comprasPessoa >= 400
+    println(resultado)
+}
