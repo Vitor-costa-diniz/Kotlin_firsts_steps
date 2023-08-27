70607aff43da08247ad913161c5d68ecbd7d4e59
Vitor-costa-diniz <78705027+Vitor-costa-diniz@users.noreply.github.com>
2023-08-27 14:36:49 -0300

    Inline funcs

---
+++ Aprendendo_Kotlin/app/src/main/java/com/vitorcosta/aprendendokotlin/FuncoesInline.kt
@@ -0,0 +1,10 @@
+package com.vitorcosta.aprendendokotlin
+
+//Inline Function
+//fun subtrair()  = println(10 - 8)
+fun subtrair(): Int  = 10 - 8
+
+fun main() {
+    val retorno = subtrair()
+    println(retorno)
+}
