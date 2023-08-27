e9dd756e5bea22d277c694a14355fcd0089b0fd8
Vitor-costa-diniz <78705027+Vitor-costa-diniz@users.noreply.github.com>
2023-08-27 10:40:39 -0300

    Var and const

---
+++ Aprendendo_Kotlin/app/src/main/java/com/vitorcosta/aprendendokotlin/VariaveisConstantes.kt
@@ -0,0 +1,22 @@
+package com.vitorcosta.aprendendokotlin
+
+fun retornaUsuario() {
+    //Executa e retorna Algo
+}
+
+class Usuario {
+
+}
+
+const val nomeUsuario = "Cristiane" //Tempo de compilação
+
+fun main() {
+    val nome = retornaUsuario() //Tempo de execução
+    val usuario = Usuario()
+
+//    val numero1 = 20
+//    val numero2 = 30
+//    var resultado = numero1 + numero2
+
+    println(nomeUsuario)
+}
